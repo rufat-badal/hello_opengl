de2d0075b324aa9186e5da7bb382f29db08d7b4e
Rufat Badal <19675055+rufat-badal@users.noreply.github.com>
2024-11-02 15:55:07 +0100

    Added framebuffer resize callback

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
+#include <cstddef>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 int main(void) {
     glfwInit();
@@ -9,4 +15,22 @@ int main(void) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+
+    // Adjusts the OpenGL viewport everytime the window gets resized.
+    // The callback function will be also called on first display.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    return 0;
 }
